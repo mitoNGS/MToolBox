3d934b4dcb956218a1f43e47444be6770dc4f967
clody23 <claudia.calabrese23@gmail.com>
2016-01-10 17:52:46 +0000

    build gsnap databases

---
+++ build_gsnap_index.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+function usage() {
+
+    cat<<EOF    
+Usage: gmap_build_index.sh -D <path_to_database> -n <database_name> -f <file.fa> -k <kmer> 
+EOF
+
+}
+
+while getopts ":hD:n:f:k:" opt; do
+	case $opt in
+		h)
+			usage
+			exit 1
+			;;
+		D)
+			database_path=$OPTARG
+			;;
+		n)
+			database_name=$OPTARG
+			;;
+		f)
+			fasta=$OPTARG
+			;;
+		k)	
+			kmer=$OPTARG
+			;;			
+		\?)
+			echo "Invalid option: -$OPTARG" >&2
+			usage
+			exit 1
+			;;
+		:)
+			echo "Option -$OPTARG requires an argument." >&2
+			usage
+			exit 1
+			;;
+	esac
+done
+
+echo 'Check if gmap is installed...'
+
+GMAP=`which gmap`
+if [ "$GMAP" == "" ]; then
+	echo "gmap is not installed. Exit."
+	exit 1
+else
+	echo 'OK!'
+fi
+
+export PATH=$GMAP:$PATH
+
+echo 'Building gmap indexes...'
+
+gmap_build -D $database_path -d $database_name $fasta -s numeric-alpha -k $kmer
+
+
+echo 'Done'
