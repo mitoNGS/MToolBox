906d15a9687b986ac58577f0d8b80eea5d7f8065
Claudia Calabrese <claudia.calabrese23@gmail.com>
2016-12-31 16:29:38 +0100

    small cange

--- test_rCRS_config.sh
+++ test_rCRS_config.sh
@@ -68,7 +68,7 @@ input_path=/path/to/inputfile/
 ##
 output_name=/path/to/output
 ##
-##OPTIONAL. Specify the FULL PATH to the list of files to be analyzed. Default is use all the files with the specified file format extension 
+##OPTIONAL. Specify the FULL PATH to the list of files to be analyzed if input_path was not defined. Default is use all the files with the specified file format extension 
 ##in the current working directory and skip this option
 ##
 list=test_list.lst
